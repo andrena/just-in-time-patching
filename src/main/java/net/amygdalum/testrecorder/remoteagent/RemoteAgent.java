e90b512e2bf2c1fc8bb7f95cf56ef4363e3e423d
Stefan Mandel <mandelst@gmail.com>
2019-12-18 05:50:28 +0100

    implemented attaching/detaching agent behavior

--- src/main/java/net/amygdalum/testrecorder/remoteagent/RemoteAgent.java
+++ src/main/java/net/amygdalum/testrecorder/remoteagent/RemoteAgent.java
@@ -4,25 +4,56 @@ import java.lang.instrument.Instrumentation;
 
 import net.bytebuddy.agent.builder.AgentBuilder;
 import net.bytebuddy.agent.builder.AgentBuilder.Transformer;
+import net.bytebuddy.agent.builder.ResettableClassFileTransformer;
 import net.bytebuddy.asm.Advice;
 import net.bytebuddy.matcher.ElementMatchers;
 
 public class RemoteAgent {
-	public static void premain(String arg, Instrumentation instrumentation) {
-		agentmain(arg, instrumentation);
-	}
+
+	public static volatile RemoteAgent agent;
 	
-	public static void agentmain(String arg, Instrumentation instrumentation) {
+	private Instrumentation instrumentation;
+	public ResettableClassFileTransformer runningTransformer;
+
+	public RemoteAgent(Instrumentation instrumentation) {
+		this.instrumentation = instrumentation;
 		Transformer transformer = (builder, type, loader, module) -> builder
 			.visit(Advice.to(Template.class).on(ElementMatchers.hasMethodName("add")));
-		new AgentBuilder.Default()
+		runningTransformer = new AgentBuilder.Default()
 			.ignore(ElementMatchers.none())
 			.disableClassFormatChanges()
+			.with(new AgentBuilder.CircularityLock.Default())
 			.with(AgentBuilder.RedefinitionStrategy.RETRANSFORMATION)
+			.with(AgentBuilder.Listener.StreamWriting.toSystemError())
 			.type(ElementMatchers.nameContains("TodoList"))
 			.transform(transformer)
 			.installOn(instrumentation);
 	}
+
+	private void shutdown() {
+		runningTransformer.reset(instrumentation, AgentBuilder.RedefinitionStrategy.RETRANSFORMATION);
+	}
+
+	public static void premain(String arg, Instrumentation instrumentation) {
+		agentmain(arg, instrumentation);
+	}
+
+	public static void agentmain(String arg, Instrumentation instrumentation) {
+		if (arg.equals("detach")) {
+			System.out.println("detaching");
+			if (agent != null) {
+				System.out.println("agent to detach exists");
+				agent.shutdown();
+				agent = null;
+			} else {
+				System.out.println("no agent to detach exists");			
+			}
+		} else {
+			System.out.println("attaching");
+			agent = new RemoteAgent(instrumentation);
+		}
+	}
+
 
 	static class Template {
 
--- src/main/java/net/amygdalum/testrecorder/remoteagent/RemoteAgentAttacher.java
+++ src/main/java/net/amygdalum/testrecorder/remoteagent/RemoteAgentAttacher.java
@@ -14,39 +14,40 @@ import sun.jvmstat.monitor.MonitoredVmUtil;
 import sun.jvmstat.monitor.VmIdentifier;
 
 /**
- * start with java net.amygdalum.testrecorder.remoteagent.RemoteAgentAttacher <agent-jar> <main-class>
+ * start with java net.amygdalum.testrecorder.remoteagent.RemoteAgentAttacher <agent-jar> <main-class> [<action>]
  * - agent jar is a jar containing a java agent
- * - main class is the main class of the java process to attach to (a list is printed 
+ * - main class is the main class of the java process to attach to (a list is printed)
+ * - action `attach` a new agent or `detach` an existing one 
  */
+@SuppressWarnings("restriction")
 public class RemoteAgentAttacher {
 	public static void main(String[] args) throws Exception {
 		File agentJar = new File(args[0]);
 		String mainClass = args[1];
-		String processId = processIdFor(mainClass);
+		String action = args.length > 2 ? args[2] : "attach";
 		try {
-			System.out.println("Attaching:" + processId);
-			ByteBuddyAgent.attach(agentJar, processId);
-			System.out.println("Detaching:" + processId);
+			String processId = processIdFor(mainClass);
+			System.out.println("Connecting for " + action + ":" + processId);
+			ByteBuddyAgent.attach(agentJar, processId, action);
+			System.out.println("Disconnecting from " + action + ":" + processId);
 		} catch (NoSuchElementException e) {
 			System.out.println(e.getMessage() + " Consider one of the list above.");
 		}
 
 	}
 
-public static String processIdFor(String name) throws MonitorException, URISyntaxException {
-	MonitoredHost monitoredHost = MonitoredHost.getMonitoredHost(new HostIdentifier((String) null));
-	Set<Integer> activeVmPids = monitoredHost.activeVms();
-	for (Integer vmPid : activeVmPids) {
-		MonitoredVm mvm = monitoredHost.getMonitoredVm(new VmIdentifier(vmPid.toString()));
-		String mvmMainClass = MonitoredVmUtil.mainClass(mvm, true);
-		System.out.println(vmPid + ":" + mvmMainClass);
-		if (mvmMainClass.contains(name)) {
-			return vmPid.toString();
+	public static String processIdFor(String name) throws MonitorException, URISyntaxException {
+		MonitoredHost monitoredHost = MonitoredHost.getMonitoredHost(new HostIdentifier((String) null));
+		Set<Integer> activeVmPids = monitoredHost.activeVms();
+		for (Integer vmPid : activeVmPids) {
+			MonitoredVm mvm = monitoredHost.getMonitoredVm(new VmIdentifier(vmPid.toString()));
+			String mvmMainClass = MonitoredVmUtil.mainClass(mvm, true);
+			System.out.println(vmPid + ":" + mvmMainClass);
+			if (mvmMainClass.contains(name)) {
+				return vmPid.toString();
+			}
 		}
+		throw new NoSuchElementException("cannot find " + name + " in java process main classes.");
+
 	}
-	throw new NoSuchElementException("cannot find " + name + " in java process main classes.");
-
-
 }
-}
-
