2024d426f15ed3802010011e5ad9d43270216851
Jan Müller <jmueller@andrena.de>
2020-01-24 11:31:41 +0100

    refac remote agent attacher

--- agent-attacher/src/main/java/de/andrena/justintime/attaching/RemoteAgentAttacher.java
+++ agent-attacher/src/main/java/de/andrena/justintime/attaching/RemoteAgentAttacher.java
@@ -2,7 +2,6 @@ package de.andrena.justintime.attaching;
 
 import java.io.File;
 import java.net.URISyntaxException;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.NoSuchElementException;
 import java.util.Set;
@@ -16,41 +15,35 @@ import sun.jvmstat.monitor.MonitoredVmUtil;
 import sun.jvmstat.monitor.VmIdentifier;
 
 /**
- * start with java de.andrena.justintime.attaching.RemoteAgentAttacher <agent-jar> <main-class> [<action>]
- * - agent jar is a jar containing a java agent
- * - main class is the main class of the java process to attach to (a list is printed)
- * - action `attach` a new agent or `detach` an existing one 
+ * start with java de.andrena.justintime.attaching.RemoteAgentAttacher
+ * <agent-jar> <main-class> [<action>] - agent jar is a jar containing a java
+ * agent - main class is the main class of the java process to attach to (a list
+ * is printed) - action `attach` a new agent or `detach` an existing one
  */
 public class RemoteAgentAttacher {
+
 	public static void main(String[] args) throws Exception {
-		Path agentJarPath = Paths.get(args[0]);
-		File agentJar = agentJarPath.toFile();
-
-		String mainClass = args[1];
+		File agentJar = Paths.get(args[0]).toFile();
+		String processId = processIdForMainClass(args[1]);
 		String action = args.length > 2 ? args[2] : "attach";
-		try {
-			String processId = processIdFor(mainClass);
-			System.out.println("Connecting for " + action + ":" + processId);
-			ByteBuddyAgent.attach(agentJar, processId, action);
-			System.out.println("Disconnecting from " + action + ":" + processId);
-		} catch (NoSuchElementException e) {
-			System.out.println(e.getMessage() + " Consider one of the list above.");
-		}
 
+		System.out.println("Connecting for " + action + ": " + processId);
+		ByteBuddyAgent.attach(agentJar, processId, action);
+		System.out.println("Disconnecting from " + action + ": " + processId);
 	}
 
-	public static String processIdFor(String name) throws MonitorException, URISyntaxException {
+	private static String processIdForMainClass(String mainClass) throws MonitorException, URISyntaxException {
 		MonitoredHost monitoredHost = MonitoredHost.getMonitoredHost(new HostIdentifier((String) null));
 		Set<Integer> activeVmPids = monitoredHost.activeVms();
 		for (Integer vmPid : activeVmPids) {
 			MonitoredVm mvm = monitoredHost.getMonitoredVm(new VmIdentifier(vmPid.toString()));
 			String mvmMainClass = MonitoredVmUtil.mainClass(mvm, true);
-			System.out.println(vmPid + ":" + mvmMainClass);
-			if (mvmMainClass.contains(name)) {
+			System.out.println("detected VM PID: " + vmPid + ", main class: \"" + mvmMainClass + "\"");
+			if (mvmMainClass.contains(mainClass)) {
 				return vmPid.toString();
 			}
 		}
-		throw new NoSuchElementException("cannot find " + name + " in java process main classes.");
 
+		throw new NoSuchElementException("cannot find main class \"" + mainClass + "\" in any java process.");
 	}
 }
