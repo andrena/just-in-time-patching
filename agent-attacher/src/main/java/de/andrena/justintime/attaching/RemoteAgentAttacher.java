5ece821b3aaa5d8c26092535c422c8978c095696
Jan Müller <jmueller@andrena.de>
2020-02-25 00:20:05 +0100

    Make Attacher also understand process id as imput

--- agent-attacher/src/main/java/de/andrena/justintime/attaching/RemoteAgentAttacher.java
+++ agent-attacher/src/main/java/de/andrena/justintime/attaching/RemoteAgentAttacher.java
@@ -15,9 +15,9 @@ import sun.jvmstat.monitor.MonitoredVmUtil;
 import sun.jvmstat.monitor.VmIdentifier;
 
 /**
- * start with java de.andrena.justintime.attaching.RemoteAgentAttacher <agent-jar> <main-class> [<action>]
+ * start with java de.andrena.justintime.attaching.RemoteAgentAttacher <agent-jar> <main-class-or-process-id> [<action>]
  * - agent jar is a jar containing a java agent
- * - main class is the main class of the java process to attach to (a list is printed) 
+ * - main class / process ID is the main class / process ID of the java process to attach to
  * - action `attach` a new agent or `detach` an existing one
  */
 public class RemoteAgentAttacher {
@@ -25,29 +25,29 @@ public class RemoteAgentAttacher {
 	public static void main(String[] args) throws Exception {
 		try {
 			File agentJar = Paths.get(args[0]).toFile();
-			String processId = processIdForMainClass(args[1]);
+			String processId = processIdFor(args[1]);
 			String action = args.length > 2 ? args[2] : "attach";
 
 			System.out.println("Connecting for " + action + ": " + processId);
 			ByteBuddyAgent.attach(agentJar, processId, action);
 			System.out.println("Disconnecting from " + action + ": " + processId);
 		} catch (NoSuchElementException e) {
-			System.out.println(e.getMessage());
+			System.err.println(e.getMessage());
 		}
 	}
 
-	private static String processIdForMainClass(String mainClass) throws MonitorException, URISyntaxException {
+	private static String processIdFor(String mainClassOrPID) throws MonitorException, URISyntaxException {
 		MonitoredHost monitoredHost = MonitoredHost.getMonitoredHost(new HostIdentifier((String) null));
 		Set<Integer> activeVmPids = monitoredHost.activeVms();
 		for (Integer vmPid : activeVmPids) {
 			MonitoredVm mvm = monitoredHost.getMonitoredVm(new VmIdentifier(vmPid.toString()));
 			String mvmMainClass = MonitoredVmUtil.mainClass(mvm, true);
 			System.out.println("detected VM PID: " + vmPid + ", main class: \"" + mvmMainClass + "\"");
-			if (mvmMainClass.contains(mainClass)) {
+			if (mvmMainClass.contains(mainClassOrPID) || mainClassOrPID.equals("" + vmPid)) {
 				return vmPid.toString();
 			}
 		}
 
-		throw new NoSuchElementException("cannot find main class \"" + mainClass + "\" in any java process.");
+		throw new NoSuchElementException("cannot find any java process with main class or process ID \"" + mainClassOrPID + "\".");
 	}
 }
